c91f29141b18089be83f6c9791b6ea33e04e1385
Roman Abramenko <abramenkoroman22@gmail.com>
2023-05-02 14:40:33 +0300

    auth

--- services/accounts/internal/interfaces/api/rest/middleware.go
+++ services/accounts/internal/interfaces/api/rest/middleware.go
@@ -7,7 +7,7 @@ import (
 )
 
 func corsMiddleware(c *gin.Context) {
-	c.Header("Access-Control-Allow-Origin", "*")
+	c.Header("Access-Control-Allow-Origin", "http://localhost:19006")
 	c.Header("Access-Control-Allow-Methods", "*")
 	c.Header("Access-Control-Allow-Headers", "*")
 	c.Header("Content-Type", "application/json")
